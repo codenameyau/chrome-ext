ab67a756898a238bfd794bf51baf3a5e07465a1d
Jorge Yau <jyau01@citymail.cuny.edu>
2016-06-19 10:09:54 -0400

    Added most styling and html

--- multi-links/script.js
+++ multi-links/script.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log(1);
+var linksTextarea = document.getElementById('textarea-links');
+var openLinksBtn = document.getElementById('open-links-btn');
+
+function saveUrls(e) {
+  // User hit ctrl + v
+  if (e.ctrlKey && e.keyCode === 86) {
+    var links = linksTextarea.value.trim().split('\n');
+    chrome.storage.sync.set({ 'links': links });
+    linksTextarea.value = links.join('\n') + '\n';
+  }
+}
+
+function openUrls(e) {
+  debugger;
+}
+
+linksTextarea.addEventListener('keyup', saveUrls);
+openLinksBtn.addEventListener('click', openUrls);
+
+chrome.storage.onChanged.addListener(function(changes, namespace) {
+  for (key in changes) {
+    var storageChanges = changes[key];
+  }
+})
